05932babdbacbac9eaee4b333fb0956d904faf9c
joserendonu <angelemilio4444@gmail.com>
2026-03-24 12:33:55 -0500

    Despliegue web

---
+++ demo/src/main/java/com/prueba/demo/HolaController.java
@@ -0,0 +1,13 @@
+package com.prueba.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/")
+    public String hola() {
+        return "Backend funcionando 🚀";
+    }
+}
